16c2caf2c47d5ebec368b58c7e87ec6c3925dd64
maven0571 <55068667+maven0571@users.noreply.github.com>
2020-09-24 17:50:37 +0300

    Update test.js

--- test.js
+++ test.js
@@ -3,30 +3,37 @@ const client = new Discord.Client();
 const ArcaClient = require("./src/arcadb/index.js");
 
 client.on("ready", () => {
-	console.log("Hello, Discord!");
+	console.log("Use Arcadb xd");
 });
 
 client.on("message", message => {
-	if(message.content === "kaydet") {
-		ArcaClient.requestSet(`${message.guild.id}_kaydet`, "veri kaydettim", message.member);
+	if(message.content === "save") {
+		ArcaClient.requestSet(`${message.guild.id}_save`, "done", message.member); // User data
+		ArcaClient.requestSet(`${message.guild.id}_save`, "done", message.guild); // Server data
 	};
-	if(message.content === "ekle") {
-		ArcaClient.requestAdd(`${message.guild.id}_kaydet`, "veri kaydettim", message.member);
+	if(message.content === "add") {
+		ArcaClient.requestAdd(`${message.guild.id}_save`, "done", message.member); // User data
+		ArcaClient.requestAdd(`${message.guild.id}_save`, "done", message.guild); // Server data
 	};
-	if(message.content === "sil") {
-		ArcaClient.requestDelete(`${message.guild.id}_kaydet`, message.member);
+	if(message.content === "delete") {
+		ArcaClient.requestDelete(`${message.guild.id}_save`, message.member); // User data
+		ArcaClient.requestDelete(`${message.guild.id}_save`, message.guild); // Server data
 	};
-	if(message.content === "sıfırla") {
-		ArcaClient.requestRemove(message.member);
+	if(message.content === "reset") {
+		ArcaClient.requestRemove(message.member); // User data
+		ArcaClient.requestRemove(message.guild); // Server data
 	};
-	if(message.content === "getir") {
-		console.log(ArcaClient.requestGet(`${message.guild.id}_kaydet`, message.member));
+	if(message.content === "get") {
+		console.log(ArcaClient.requestGet(`${message.guild.id}_save`, message.member)); // User data
+		console.log(ArcaClient.requestGet(`${message.guild.id}_save`, message.guild)); // Server data
 	};
-	if(message.content === "kontrol") {
-		console.log(ArcaClient.request(`${message.guild.id}_kaydet`, message.member));
+	if(message.content === "check") {
+		console.log(ArcaClient.requestRun(`${message.guild.id}_save`, message.member)); // User data
+		console.log(ArcaClient.requestRun(`${message.guild.id}_save`, message.guild)); // Server data
 	};
-	if(message.content === "listele") {
-		console.log(ArcaClient.requestAll(message.member));
+	if(message.content === "list") {
+		console.log(ArcaClient.requestAll(message.member)); // User data
+		console.log(ArcaClient.requestAll(message.guild)); // Server data
 	};
 });
 
